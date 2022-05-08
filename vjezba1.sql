e3c0ff1c677bd00575b1fc5e0da49e4772f420f8
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-08 16:43:07 +0200

    Add files via upload
    dz1

---
+++ vjezba1.sql
@@ -0,0 +1,30 @@
+# c:\xampp\mysql\bin\mysql -uroot < c:\users\mzdel\documents\github\mizdelar\vjezbadatabase\vjezba1.sql
+
+
+drop database if exists vjezba1;
+create database vjezba1;
+use vjezba1;
+
+create table frizerskisalon(
+    djelatnica varchar(50),
+    korisnik varchar(50),
+    usluga varchar(50)
+);
+
+create table salonzauljepsavanje(
+    djelatnica varchar(50),
+    korisnik varchar(50),
+    usluga varchar(50)
+);
+
+create table udrugazazastituzivotinja(
+    osoba varchar(50),
+    zivotinja varchar(50),
+    prostor varchar(50)
+);
+
+create table djecjivrtic(
+    odgojnaskupina varchar(50),
+    odgajateljica varchar(50),
+    strucnasprema varchar(50)
+);
