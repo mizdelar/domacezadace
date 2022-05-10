de43c17bbab0ace1040e587d6748c787d1a6c36e
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-10 19:30:47 +0200

    Add files via upload

--- postolar.sql
+++ postolar.sql
@@ -2,8 +2,24 @@ drop database if exists postolar;
 create database postolar;
 use postolar;
 
-create table postolar(
-    obuca varchar(50),
-    korisnik varchar(50),
-    segrt varchar(50)
+create table obuca(
+    velicina varchar(50),
+    materijal varchar(50),
+    cijena varchar(50),
+    proizvodjac varchar(50),
+    nacinizrade varchar(50),
+    brojdolazakanapopravak varchar(50)
+);
+
+create table korisnik(
+    brojkomadaobucepokorisniku varchar(50),
+    ime varchar(50),
+    prezime varchar(50),
+    brojdolazakakodpostolara varchar(50)
+);
+
+create table segrt(
+    vrstapopravakakojeradi varchar(50),
+    brojsegrtapopostolaru varchar(50),
+    godineiskustvasegrta varchar(50)
 );
