6213d7f4e5d0e95c920ca9bddc57f7dbc58bbd18
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-10 19:31:23 +0200

    Add files via upload

--- urar.sql
+++ urar.sql
@@ -2,9 +2,27 @@ drop database if exists urar;
 create database urar;
 use urar;
 
-create table urar(
-    sat varchar(50),
-    korisnik varchar(50),
-    popravak varchar(50),
-    segrt varchar(50)
+create table sat(
+    materijal varchar(50),
+    proizvodjac varchar(50),
+    brojpopravakaposatu varchar(50),
+    nacinprikazavremena varchar(50)
+);
+create table korisnik(
+    ime varchar(50),
+    prezime varchar(50),
+    brojsatovapokorisniku varchar(50),
+    brojdolazakauraru varchar(50)
+);
+
+create table segrt(
+    popravcisegrta varchar(50),
+    brojsegrtapopostolaru varchar(50),
+    godineiskustvasegrta varchar(50)
+);
+create table popravak(
+    duljinatrajanjapopravka varchar(50),
+    kategorijatezinepopravka varchar(50),
+    vrstapopravka varchar(50),
+    serviserpopravak varchar(50)
 );
