cb7c6627b6bc96865a9f04ead220c4e8e9d5cb74
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-10 19:23:39 +0200

    Add files via upload

--- muzej.sql
+++ muzej.sql
@@ -2,9 +2,25 @@ drop database if exists muzej;
 create database muzej;
 use muzej;
 
-create table muzej(
-    izlozba varchar(50),
-    djelo varchar(50),
-    kustos varchar(50),
-    sponzor varchar(50)
+create table izlozba(
+    nazivizlozbe varchar(50),
+    trajanjeizlozbe varchar(50),
+    cijenaulaznicezaizlouzbu varchar(50),
+    brojizlozbiumuzeju varchar(50)
+);
+
+create table djelo(
+    naziv varchar(50),
+    autor varchar(50),
+    tema varchar(50),
+    brojdjelapoizlozbi varchar(50)
+);
+
+create table kustos(
+    ime varchar(50),
+    prezime varchar(50),
+    oib varchar(50),
+    visinaplace varchar(50),
+    brojizlozbipokustosu varchar(50),
+    duljinaradnogvremena varchar(50)
 );
