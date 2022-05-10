1bdcfc9dcff1aa2f9581def376266bd6f7ff765a
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-10 19:31:04 +0200

    Add files via upload

--- samostan.sql
+++ samostan.sql
@@ -2,8 +2,24 @@ drop database if exists samostan;
 create database samostan;
 use samostan;
 
-create table samostan(
-    svecenik varchar(50),
-    posao varchar(50),
-    nadredjenisvecenik varchar(50)
+create table svecenik(
+    ime varchar(50),
+    prezime varchar(50),
+    oib varchar(50),
+    visinaplace varchar(50),
+    brojsvecenikaposamostanu varchar(50),
+    brojnadredjenihsvecenikaposveceniku varchar(50)
 );
+
+create table posao(
+    duljinatrajanjaposla varchar(50),
+    brojsvecenikapoposlu varchar(50),
+    kategorijatezineposla varchar(50)
+    );
+
+    create table nadredjenisvecenik(
+        ime varchar(50),
+        prezime varchar(50),
+        oib varchar(50),
+        brojsvecenikaponadredjenomsveceniku varchar(50)
+    );
