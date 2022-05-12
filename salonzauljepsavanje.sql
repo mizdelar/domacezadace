340c8ec68b3b841ef71829dee85c459c174e69ae
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-12 20:38:07 +0200

    Create salonzauljepsavanje.sql

---
+++ salonzauljepsavanje.sql
@@ -0,0 +1,10 @@
+drop database if exists salonzauljepsavanje;
+create database salonzauljepsavanje;
+use salonzauljepsavanje;
+
+create table salonzauljepsavanje(
+    djelatnica varchar(50),
+    korisnik varchar (50),
+    usluga varchar(50)
+
+);
