c54916695cda3101dc9ef56dc4d550b90ec9a46e
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-12 20:37:54 +0200

    Create frizerskisalon.sql

---
+++ frizerskisalon.sql
@@ -0,0 +1,9 @@
+drop database if exists frizerskisalon;
+create database frizerskisalon;
+use frizerskisalon;
+
+create table frizerskisalon(
+    djelatnica varchar(50),
+    korisnik varchar(50),
+    usluga varchar(50)
+);
