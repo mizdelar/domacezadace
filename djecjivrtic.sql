2f00641c239aff273b415d9694f3081a7bbf77b0
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-12 20:37:47 +0200

    Create djecjivrtic.sql

---
+++ djecjivrtic.sql
@@ -0,0 +1,10 @@
+drop database if exists djecjivrtic;
+create database djecjivrtic;
+use djecjivrtic;
+
+create table djecjivrtic(
+    odgojnaskupina varchar(50),
+    odgajateljica varchar(50),
+    strucnasprema varchar(50)
+
+);
