032d0e94fe7a89041393128bac7f77de9c3a445c
mizdelar <104709269+mizdelar@users.noreply.github.com>
2022-05-21 09:39:23 +0200

    Edited

--- doktorskaordinacija.sql
+++
@@ -1,9 +0,0 @@
-drop database if exists doktorskaordinacija;
-create database doktorskaordinacija;
-use doktorskaordinacija;
-
-create table doktorskaordinacija(
-    doktor varchar(50),
-    pacijent varchar(50),
-    maedicinskasestra varchar(50)
-);
--- udrugazazastituzivotinja.sql
+++
@@ -1,9 +0,0 @@
-drop database if exists udrugazazastituzivotinja;
-create database udrugazazastituzivotinja;
-use udrugazazastituzivotinja;
-
-create table udrugazazastituzivotinja(
-    osoba varchar(50),
-    sticenik varchar(50),
-    prostor varchar(50)
-);
